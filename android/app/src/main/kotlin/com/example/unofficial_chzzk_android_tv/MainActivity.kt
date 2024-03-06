bf06a88b426abd53de39205628bec389ece6738c
BGPark <escaper.bgp@gmail.com>
2024-03-06 16:35:37 +0900

    remove all files

--- android/app/src/main/kotlin/com/example/unofficial_chzzk_android_tv/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.escaperbgp.unofficial_chzzk_android_tv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/constants/dimensions.dart
+++
@@ -1,5 +0,0 @@
-class Dimensions {
-  static const double liveContainerHeight = 220.0;
-  static const double liveThumbnailWidth = 245.0;
-  static const double followingInfoCardWidth = 220.0;
-}
